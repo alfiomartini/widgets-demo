cba0216bfde4e0981506adc513be8a308f24ce92
Alfio Martini <alfio.martini@gmail.com>
2021-01-12 16:06:40 -0300

    Fixed bug w.r.t. highlighting current menu option (1st version)

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
 
-const Link = ({className, href, children}) => {
-  const onClick = (event) => {
+class Link extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+         path:window.location.pathname,
+         href:this.props.href
+        }
+  }
+ 
+  onClick = (event) => {
+    // console.log('wlp, href', window.location.pathname, this.props.href);
     // normal browser behaviour
     if (event.metaKey || event.ctrlKey){
       return;
     }
     // prevents a full page reload
     event.preventDefault();
-    window.history.pushState({},'', href);
+    window.history.pushState({},'', this.props.href);
     const navEvent = new PopStateEvent('popstate');
-    window.dispatchEvent(navEvent)
+    window.dispatchEvent(navEvent);
   }
-  return (
-    <a onClick = {onClick}
-    className={`${className} link`} href={href}>{children}
-    </a>
-  ) 
+
+  render(){
+    return (
+      <a onClick = {() => {
+          this.onClick(); this.setState({path:window.location.pathname});
+        }}
+        className={`${window.location.pathname=== this.props.href?'active':''} link`} 
+        href={this.props.href}>
+        {this.props.children}
+      </a>
+    ) 
+  }
 }
 
 export default Link;
--- src/components/NavbarCss.jsx
+++ src/components/NavbarCss.jsx
@@ -3,19 +3,19 @@ import Link from './Link';
 
 import './css/NavbarCss.css';
 const NavbarCss = ({options, basePath}) => {
-
+  
   const menuList = options.map((item, index) => {
     return (
       <li key={index}
       > 
         <Link href={basePath + item.path}
-        className={`${window.location.pathname=== basePath+item.path?'active':''}`}
         >
           {item.option}
         </Link>
       </li>
     )
-  })
+  });
+  console.log('menu list', menuList.length)
   return(
     <ul className='menu-list'>
       {menuList}
